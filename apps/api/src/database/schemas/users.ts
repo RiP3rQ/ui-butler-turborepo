3204c78a03f990864c163db51b1522fd47a1fe15
Rafał Pompa <rafalpompa2000@gmail.com>
2024-12-07 16:36:43 +0100

    feat: downgraded eslint version

--- apps/api/src/database/schemas/users.ts
+++ apps/api/src/database/schemas/users.ts
@@ -1,12 +1,12 @@
-import { relations } from "drizzle-orm";
-import { integer, pgTable, serial, text } from "drizzle-orm/pg-core";
+import { relations } from 'drizzle-orm';
+import { integer, pgTable, serial, text } from 'drizzle-orm/pg-core';
 
-export const users = pgTable("users", {
-  id: serial("id").primaryKey(),
-  email: text("email").unique(),
-  username: text("username"), // backfill
-  password: text("password").notNull(),
-  refreshToken: text("refresh_token"),
+export const users = pgTable('users', {
+  id: serial('id').primaryKey(),
+  email: text('email').unique(),
+  username: text('username'), // backfill
+  password: text('password').notNull(),
+  refreshToken: text('refresh_token'),
 });
 
 export type User = typeof users.$inferSelect;
@@ -16,12 +16,12 @@ export const userRelations = relations(users, ({ one }) => ({
   profile: one(profile),
 }));
 
-export const profile = pgTable("profile", {
-  id: serial("id").primaryKey(),
-  age: integer("age"),
-  biography: text("biography"),
-  userId: integer("user_id").references(() => users.id),
-  avatar: text("avatar_url"),
+export const profile = pgTable('profile', {
+  id: serial('id').primaryKey(),
+  age: integer('age'),
+  biography: text('biography'),
+  userId: integer('user_id').references(() => users.id),
+  avatar: text('avatar_url'),
 });
 
 export const profileRelations = relations(profile, ({ one }) => ({
