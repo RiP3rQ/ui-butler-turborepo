0e468d3ccd36955ff5583a8a0c2caabff61d88e1
Rafał Pompa <rafalpompa2000@gmail.com>
2024-12-09 14:53:13 +0100

    feat: updated drizzle schema

--- apps/api/src/database/merged-schemas.ts
+++ apps/api/src/database/merged-schemas.ts
@@ -3,6 +3,7 @@ import * as analyticsSchema from './schemas/analytics';
 import * as credentialsSchema from './schemas/credentials';
 import * as workflowExecutionsSchema from './schemas/workflow-executions';
 import * as workflowsSchema from './schemas/workflows';
+import * as projectsSchema from './schemas/projects';
 import { NodePgDatabase } from 'drizzle-orm/node-postgres';
 
 export const mergedSchemas = {
@@ -11,6 +12,7 @@ export const mergedSchemas = {
   ...credentialsSchema,
   ...workflowExecutionsSchema,
   ...workflowsSchema,
+  ...projectsSchema,
 };
 export type DatabaseSchemas = typeof mergedSchemas;
 export type DrizzleDatabase = NodePgDatabase<DatabaseSchemas>;
