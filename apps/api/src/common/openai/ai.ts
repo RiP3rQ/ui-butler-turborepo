5da9d78e8d9a8c04b41d9d7dad73f6459270bf57
Rafał Pompa <rafalpompa2000@gmail.com>
2024-12-12 14:58:42 +0100

    feat: api anlytics + ai fixes

--- apps/api/src/common/openai/ai.ts
+++ apps/api/src/common/openai/ai.ts
@@ -4,4 +4,4 @@ const google = createGoogleGenerativeAI({
   // custom settings
 });
 
-export const GEMINI_MODEL = google('gemini-1.5-pro-latest');
+export const GEMINI_MODEL = google('gemini-1.5-flash-latest');
