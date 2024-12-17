21e3f56fc0e969846d1c974dfbc6a65908407a0b
Rafał Pompa <rafalpompa2000@gmail.com>
2024-12-17 15:12:08 +0100

    feat: confirmation modal done

--- apps/web/src/components/component-viewer/display-action-button.tsx
+++ apps/web/src/components/component-viewer/display-action-button.tsx
@@ -3,6 +3,8 @@ import type { UseMutateFunction } from "@tanstack/react-query";
 import type { CodeType, ComponentType } from "@repo/types";
 import { Button } from "@repo/ui/components/ui/button";
 import { type GenerateCodeFunctionProps } from "@/actions/components/generate-code-function";
+import { useConfirmationModalStore } from "@/store/confirmation-modal-store";
+import { getErrorMessage } from "@/lib/get-error-message";
 
 interface DisplayActionButtonProps {
   action?: {
@@ -24,18 +26,46 @@ export function DisplayActionButton({
   componentId,
   generateCodeMutation,
 }: Readonly<DisplayActionButtonProps>): JSX.Element | null {
+  const {
+    setIsModalOpen,
+    setIsPending,
+    setSaveButtonDisabled,
+    setConfirmationModalBasicState,
+  } = useConfirmationModalStore();
+
+  function handleGenerateCodeWithConfirmation() {
+    setConfirmationModalBasicState({
+      isModalOpen: true,
+      modalTitle: "Confirm code generation",
+      modalSubtitle: `Are you sure you want to generate code for type ${type}?`,
+      saveButtonText: "Generate",
+      cancelButtonText: "Cancel",
+      saveButtonFunction: () => {
+        try {
+          setIsPending(true);
+          setSaveButtonDisabled(true);
+          generateCodeMutation({
+            componentId: Number(componentId),
+            codeType: type,
+          });
+        } catch (e) {
+          throw new Error(getErrorMessage(e));
+        } finally {
+          setIsPending(false);
+          setSaveButtonDisabled(false);
+          setIsModalOpen(false);
+        }
+      },
+    });
+  }
+
   if (!action?.title || !action.icon) return null;
 
   return (
     <Button
       variant="outline"
       size="sm"
-      onClick={() => {
-        generateCodeMutation({
-          componentId: Number(componentId),
-          codeType: type,
-        });
-      }}
+      onClick={handleGenerateCodeWithConfirmation}
       className="mx-2"
     >
       <action.icon className="size-4 mr-2" />
