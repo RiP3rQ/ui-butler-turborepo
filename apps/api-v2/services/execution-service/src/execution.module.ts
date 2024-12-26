9101db2e06f5a66a79c6d7cbe6a2633ac4a2223b
Rafał Pompa <rafalpompa2000@gmail.com>
2024-12-26 17:15:59 +0100

    feat: execution microservice refactored with errors

--- apps/api-v2/services/execution-service/src/execution.module.ts
+++ apps/api-v2/services/execution-service/src/execution.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from '@app/database';
 import { ExecutionsController } from './execution.controller';
 import { ExecutionsService } from './execution.service';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { GrpcErrorInterceptor } from '@app/common';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [ExecutionsController],
-  providers: [ExecutionsService],
+  providers: [
+    ExecutionsService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: GrpcErrorInterceptor,
+    },
+  ],
 })
 export class ExecutionsModule {}
