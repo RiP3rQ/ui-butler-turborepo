d545e5ac38d9c066dc90270d7883e4b24047d557
Rafał Pompa <rafalpompa2000@gmail.com>
2025-01-19 19:55:50 +0100

    feat: workflows service eslint + ts errors fixed

--- apps/api-v2/services/execution-service/.eslintrc.js
+++ apps/api-v2/services/execution-service/.eslintrc.js
@@ -5,5 +5,9 @@ module.exports = {
     tsconfigRootDir: __dirname,
     project: ['./tsconfig.json'],
   },
-  ignorePatterns: ['.eslintrc.js', 'dist', 'node_modules'],
+  rules: {
+    'no-console': 'off',
+    '@typescript-eslint/no-extraneous-class': 'off',
+  },
+  ignorePatterns: ['dist/**/*', 'node_modules/**/*'],
 };
