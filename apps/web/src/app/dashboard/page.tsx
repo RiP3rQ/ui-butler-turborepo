41ad5e4ef3a60e5a03f58c77e5b4469d7a336ce6
Rafał Pompa <rafalpompa2000@gmail.com>
2025-01-27 23:21:31 +0100

    feat: improve dashboard layout and enhance project card design

--- apps/web/src/app/dashboard/page.tsx
+++ apps/web/src/app/dashboard/page.tsx
@@ -20,9 +20,9 @@ export default async function DashboardPage(): Promise<JSX.Element> {
   ]);
 
   return (
-    <div className="flex flex-1 flex-col h-full gap-2 mx-auto max-w-6xl mt-4">
-      <div className="flex justify-between">
-        <h1 className="text-3xl font-bold">Your Dashboard</h1>
+    <div className="flex flex-1 flex-col h-full gap-4 mx-auto max-w-6xl mt-4 px-4 sm:px-6 lg:px-8">
+      <div className="flex justify-between items-center">
+        <h1 className="text-3xl font-bold text-white">Your Dashboard</h1>
       </div>
       <div className="h-full py-6 flex flex-col space-y-8">
         <DashboardStatCards initialData={dashboardStatCardsValues} />
