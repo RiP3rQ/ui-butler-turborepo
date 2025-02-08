e67d4dba52134fe82dc45e8fd00552cc806a0f55
Rafał Pompa <rafalpompa2000@gmail.com>
2025-02-08 17:02:41 +0100

    feat: config module init + bullmq module ini

--- apps/api-v2/services/execution-service/src/execution.module.ts
+++ apps/api-v2/services/execution-service/src/execution.module.ts
@@ -5,16 +5,34 @@ import { APP_INTERCEPTOR } from '@nestjs/core';
 import { BullModule } from '@nestjs/bullmq';
 import { ExecutionsController } from './execution.controller';
 import { ExecutionsService } from './execution.service';
+import { ConfigModule } from '@nestjs/config';
+import Joi from 'joi';
 
 @Module({
   imports: [
     DatabaseModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env', '.env.local'],
+      validationSchema: Joi.object({
+        DATABASE_URL: Joi.string().required(),
+        GOOGLE_GENERATIVE_AI_API_KEY: Joi.string().required(),
+        EXECUTION_SERVICE_HOST: Joi.string().required(),
+        EXECUTION_SERVICE_PORT: Joi.number().required(),
+        REDIS_HOST: Joi.string().required(),
+        REDIS_PORT: Joi.number().required(),
+      }),
+    }),
     BullModule.forRoot({
       connection: {
-        host: 'localhost',
-        port: 6379,
+        host: process.env.REDIS_HOST || 'localhost',
+        port: parseInt(process.env.REDIS_PORT ?? '6379', 10),
+      },
+      defaultJobOptions: {
+        attempts: 3,
       },
     }),
+    BullModule.registerQueue({ name: 'executions' }),
   ],
   controllers: [ExecutionsController],
   providers: [
