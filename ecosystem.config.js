f0962c5d15b122dda6151b41aa672cc966b78a8e
Rafał Pompa <rafalpompa2000@gmail.com>
2025-02-14 22:45:13 +0100

    feat: add VPS deployment instructions and update ecosystem configuration for PM2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,14 +10,17 @@ module.exports = {
     production: {
       key: "uibutler.pem",
       user: "uibutler",
-      host: "uibulter",
+      host: "20.215.33.2",
       ref: "origin/main",
-      repo: "https://github.com/RiP3rQ/ui-butler-turborepo.git",
+      repo: "git@github.com:RiP3rQ/ui-butler-turborepo.git",
       path: "/home/uibutler",
-      "pre-deploy-local": "",
+      "pre-setup": "ssh -T git@github.com",
+      "post-setup":
+        "echo 'commands or a script path to be run on the host after cloning the repo'",
+      "pre-deploy": "pm2 startOrRestart ecosystem.json --env production",
+      "pre-deploy-local": "echo 'This is a local executed command'",
       "post-deploy":
-        "source ~/.nvm/nvm.sh && npm install -g pnpm@10.2.1 && pnpm install && pnpm run build:backend && pm2 reload ecosystem.config.js --env production",
-      "pre-setup": "",
+        "source ~/.nvm/nvm.sh && pnpm install && pnpm run build:backend && pm2 reload ecosystem.config.js --env production",
       ssh_options: "ForwardAgent=yes",
     },
   },
