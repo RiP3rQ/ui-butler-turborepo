2f8cb1634fded08ed7a606fde01e90447447b116
Rafał Pompa <rafalpompa2000@gmail.com>
2025-02-14 23:32:42 +0100

    feat: update ecosystem.config.js for consistent quoting in PNPM_HOME and PATH exports

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -30,8 +30,8 @@ module.exports = {
         "source ~/.nvm/nvm.sh && " +
         "export NVM_DIR=$HOME/.nvm && " +
         "[ -s $NVM_DIR/nvm.sh ] && . $NVM_DIR/nvm.sh && " +
-        'export PNPM_HOME="/home/uibutler/.local/share/pnpm" && ' +
-        'export PATH="$PNPM_HOME:$PATH" && ' +
+        "export PNPM_HOME='/home/uibutler/.local/share/pnpm' && " +
+        "export PATH='$PNPM_HOME:$PATH'' && " +
         "export NODE_OPTIONS='--max_old_space_size=2048' && " +
         "/home/uibutler/.local/share/pnpm/pnpm install --frozen-lockfile && " +
         "cd /home/uibutler/current && " +
