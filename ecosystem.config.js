cf5aed392bcaf05bedf26bff37eac18fb366043d
Rafał Pompa <rafalpompa2000@gmail.com>
2025-02-14 23:38:11 +0100

    feat: update ecosystem.config.js to allow flexible PNPM installation without frozen lockfile

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -33,7 +33,7 @@ module.exports = {
         "npm install -g pnpm && " +
         "export NODE_OPTIONS='--max_old_space_size=2048' && " +
         "cd /home/uibutler/current && " +
-        "pnpm install --frozen-lockfile && " +
+        "pnpm install && " +
         "pnpm run build:types && " +
         "pnpm run bootstrap && " +
         "pnpm run build:backend && " +
