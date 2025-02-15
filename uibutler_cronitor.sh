f448fdb2f40ae120e7aaabb3fcb966d75450fafc
Rafał Pompa <rafalpompa2000@gmail.com>
2025-02-16 00:30:14 +0100

    feat: update cronitor script to use pm2 path variable and improve service reload process

--- uibutler_cronitor.sh
+++ uibutler_cronitor.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
+# Find the full path to pm2 using 'which pm2' when logged in as the same user
+PM2_PATH="/home/uibutler/.nvm/versions/node/v22.14.0/bin/pm2"  # This path may vary on your system
+
 echo "Starting the daily restart of the uibutler backend microservices --- $(date)"
-echo "Stopping the uibutler backend microservices"
-pm2 stock backend
-echo "Waiting for 10 seconds"
-sleep 10
-echo "Starting the uibutler backend microservices using pm2"
-pm2 start 'pnpm run start:backend' --name backend
+echo "Going to project location"
+cd /home/uibutler/app
+echo "Reload the uibutler backend microservices"
+$PM2_PATH reload backend
 echo "Waiting for 15 seconds"
 sleep 15
 echo "Validating the uibutler backend microservices"
-pm2 list
+$PM2_PATH list
 echo "Displaying 300 lines of the uibutler backend microservices logs"
-pm2 logs backend --lines 300
+$PM2_PATH logs backend --lines 300 --nostream
 echo "Ending the daily restart of the uibutler backend microservices --- $(date)"
