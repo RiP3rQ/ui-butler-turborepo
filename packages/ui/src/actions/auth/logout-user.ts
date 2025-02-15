3ee2a94eac22eb0ead102c5e370afec9768bc5a3
Rafał Pompa <rafalpompa2000@gmail.com>
2025-02-15 23:07:30 +0100

    feat: update logoutUser to set cookies for immediate deletion

--- packages/ui/src/actions/auth/logout-user.ts
+++ packages/ui/src/actions/auth/logout-user.ts
@@ -5,7 +5,13 @@ import { cookies } from "next/headers";
 export async function logoutUser(): Promise<void> {
   const cookieStore = await cookies();
   // Remove the access token from the cookie
-  cookieStore.delete("Authentication");
+  cookieStore.set("Authentication", "this_will_be_deleted_anyways", {
+    maxAge: 0,
+    expires: new Date(0),
+  });
   // Remove the refresh token from the cookie
-  cookieStore.delete("Refresh");
+  cookieStore.set("Refresh", "this_will_be_deleted_anyways", {
+    maxAge: 0,
+    expires: new Date(0),
+  });
 }
